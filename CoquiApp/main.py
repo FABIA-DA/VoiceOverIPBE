f23d057cdf167558395a4afed238644753efce57
LukasPerger <l.perger@HTBLALeonding.ac.at>
2025-08-09 21:07:02 +0200

    dockerization coqui app

--- CoquiApp/main.py
+++ CoquiApp/main.py
@@ -1,21 +1,15 @@
-﻿import torch
-#import sounddevice as sd
+﻿import os
+import torch
 import csv
 from TTS.api import TTS
+
+os.makedirs("out", exist_ok=True)
 
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Init TTS
 tts = TTS("tts_models/de/css10/vits-neon").to(device)
-
-# wav = tts.tts(text="Grüß Gott und willkomen bei Fabia!")
-#
-# sd.play(wav)
-# sd.wait()
-
-# Text to speech to a file
-#tts.tts_to_file(text="Grüß Gott und willkomen bei Fabia!", file_path="output.wav")
 
 with open("Phrases.csv", newline='', encoding="utf-8") as csvfile:
   first = True
@@ -26,3 +20,5 @@ with open("Phrases.csv", newline='', encoding="utf-8") as csvfile:
       continue
     audio_path  = f"./out/{row[0]}.wav"
     tts.tts_to_file(text=row[2], file_path=audio_path)
+    print(f"Created file {audio_path}")
+print("Done...")
