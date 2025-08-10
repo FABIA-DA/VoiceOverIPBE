ac24b3afe956fa3832a51e2f6be838022aeca0fa
LukasPerger <l.perger@HTBLALeonding.onmicrosoft.com>
2025-08-11 01:24:26 +0200

    update docker compose

--- ARIApp/build.gradle.kts
+++ ARIApp/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("java")
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "at.htlleonding.fabia"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("at.htlleonding.fabia.Main")
+}
 
 repositories {
     mavenCentral()
@@ -18,6 +24,21 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "at.htlleonding.fabia.Main"
+    }
+}
+
+tasks {
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        archiveClassifier.set("")
+        manifest {
+            attributes["Main-Class"] = "at.htlleonding.fabia.Main"
+        }
+    }
 }
 
 tasks.test {
--- ARIApp/src/main/java/at/htlleonding/fabia/Main.java
+++ ARIApp/src/main/java/at/htlleonding/fabia/Main.java
@@ -2,18 +2,16 @@ package at.htlleonding.fabia;
 
 import ch.loway.oss.ari4java.ARI;
 import ch.loway.oss.ari4java.AriVersion;
+import ch.loway.oss.ari4java.generated.models.ChannelDtmfReceived;
 import ch.loway.oss.ari4java.generated.models.Message;
 import ch.loway.oss.ari4java.tools.ARIException;
 import ch.loway.oss.ari4java.tools.AriConnectionEvent;
 import ch.loway.oss.ari4java.tools.AriWSCallback;
 import ch.loway.oss.ari4java.tools.RestException;
 
-import java.io.IOException;
-import java.nio.file.Path;
-
 public class Main {
     public static void main(String[] args) throws ARIException, InterruptedException {
-        String ariUrl = "http://localhost:8088";
+        String ariUrl = "http://asterisk:8088";
         String ariUser = "ariuser";
         String ariPass = "aripass";
         String stasisApp = "my-ari-app";
@@ -25,15 +23,29 @@ public class Main {
                 .setSubscribeAll(true)
                 .execute(new AriWSCallback<Message>() {
             @Override
-            public void onSuccess(Message result) {
-                System.out.println("Success: " + result.toString());
-                AudioUploader.sendFile(
-                        "http://localhost:8000/transcribe",
-                        Path.of("ARIApp/audio/Ragebait.m4a").toString())
-                                .subscribe(
-                                        res -> System.out.println("Transcribed text:" + res),
-                                        err -> System.err.println("Error during file upload: " + err.getMessage()
-                                ));
+            public void onSuccess(Message event) {
+                System.out.println("Success: " + event.toString());
+
+                if(event.getType().equals("ChannelDtmfReceived")){
+                    String digit = ((ChannelDtmfReceived) event).getDigit();
+                    System.out.println("Digit pressed: " + digit);
+                }
+
+                String channelId = event.getAsterisk_id();
+                try {
+                    ari.channels()
+                            .play(channelId, "sound:greeting")
+                            .execute();
+                } catch(RestException e) {
+                    System.err.println("Error playing audio: " + e.getMessage());
+                }
+//                AudioUploader.sendFile(
+//                        "http://localhost:8000/transcribe",
+//                        Path.of("ARIApp/audio/Ragebait.m4a").toString())
+//                                .subscribe(
+//                                        res -> System.out.println("Transcribed text:" + res),
+//                                        err -> System.err.println("Error during file upload: " + err.getMessage()
+//                                ));
             }
 
             @Override
